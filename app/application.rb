40b800c6cdf2e3403da96feb0932a7adb36000ac
Stephen Olson <stephenolson@gmail.com>
2019-08-04 01:02:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,16 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
+    if req.path.match(/songs/)
+ 
+      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
+ 
+      resp.write song.artist
+    end
+ 
+    resp.finish
+  end
     
     
   end
