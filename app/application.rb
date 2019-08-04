0280b8baac93eacbc39de00b9c4240ef101a8384
Stephen Olson <stephenolson@gmail.com>
2019-08-04 01:03:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     if req.path.match(/items/)
  
       item_name = req.path.split("/items/").last 
-      if item = @@songs.find{|s| s.title == song_title}
+      if item = @@items.find{|s| s.title == song_title}
  
       resp.write song.artist
     end
