2d5be18643fa62c678ae4498ed18f86a1d52726f
Stephen Olson <stephenolson@gmail.com>
2019-08-04 01:04:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
         if item = @@items.find{|i| i.name == item_name}
           resp.write item.price
     else 
-      resp.write
+      resp.write "Item not found"
       resp.status = 400
     end
  
